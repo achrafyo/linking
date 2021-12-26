a3683e98047f1dec86ffcf69873037fc6125067f
Achraf IGHID <31323958+achrafyo@users.noreply.github.com>
2021-12-26 16:18:27 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome" />
         <p className="description">
          <p><span style="color: #00ccff;"><strong><a href="https://ar.seovalide.com/backlinks/">شراء باك لينك</a><br /><a href="https://seovalide.com/obtenir-des-backlinks/">obtenir des backlinks</a><br /><a href="https://seovalide.com/acheter-des-backlinks/">acheter des backlinks</a><br /><a href="https://service.seovalide.com/">خدمات سيو</a><br /><a href="https://services.seovalide.com/">services seo</a><br /><a href="https://seovalide.com/">seo Maroc</a><br /><a href="https://ar.seovalide.com/">أفضل شركة خدمات سيو</a><br /><br /></strong></span></p>
 <p><span style="color: #00ccff;"><strong><a style="color: #00ccff;" href="https://canadanouvelles.com/spotify-premium-gratuit/">Spotify Premium Apk</a></strong></span></p>
